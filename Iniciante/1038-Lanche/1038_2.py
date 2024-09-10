1af26093b5f15a9104d122b2db4c22e5025a5536
Paulo Anjos <pauloanjos.ufopa@gmail.com>
2024-09-10 12:00:42 -0300

    Update 1038_2.py

--- Iniciante/1038-Lanche/1038_2.py
+++ Iniciante/1038-Lanche/1038_2.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-codigo, quantidade = [int(x) for x in input().strip().split(' ')]
+codigo, quantidade = map(int,input().split())
 precos = [4.00, 4.50, 5.00, 2.00, 1.50]
-
 total = quantidade * precos[codigo - 1]
-
 print(f"Total: R$ {total:.2f}")
