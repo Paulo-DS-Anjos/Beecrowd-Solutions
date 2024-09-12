76f991c9afc6f8f5368636f6f647682dc417bc77
Paulo Anjos <pauloanjos.ufopa@gmail.com>
2024-09-12 14:25:36 -0300

    Update 1047.py

--- Iniciante/1047-TempoDeJogoComMinutos/1047.py
+++ Iniciante/1047-TempoDeJogoComMinutos/1047.py
@@ -1,1 +1,7 @@
- 
+# -*- coding: utf-8 -*-
+hora_inicial,minuto_inicial,hora_final,minuto_final = map(int,input().split())
+final = hora_final * 60 + minuto_final
+inicial = hora_inicial * 60 + minuto_inicial
+if final <= inicial:
+    final += 24 * 60
+print(f'O JOGO DUROU {(final - inicial)//60} HORA(S) E {(final - inicial)%60 } MINUTO(S)')
