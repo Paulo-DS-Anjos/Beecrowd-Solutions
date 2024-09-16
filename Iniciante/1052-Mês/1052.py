ca6f1ba759fc467108d3fd4267226f4d1f5421bd
Paulo Anjos <pauloanjos.ufopa@gmail.com>
2024-09-16 17:11:21 -0300

    Add files via upload

---
+++ Iniciante/1052-Mês/1052.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+mes = int(input())
+meses = ['January', 'February', 'March', 'April', 'May', 'June',
+         'July', 'August', 'September', 'October', 'November', 'December']
+print(meses[mes - 1])
